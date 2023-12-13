5cb10adef1997f688122b98b138f847241d86243
FirNest <128029842+FirNest@users.noreply.github.com>
2023-12-13 18:49:51 +0300

    Update task_7.sh
    Correct work with 0.0.0.0

--- task_7.sh
+++ task_7.sh
@@ -3,6 +3,8 @@
 valid_ipv4() {
     local ip="$1"
     err_msg='IP address is invalid'
+    # Check if the IP address is not 0.0.0.0
+    [[ "$ip" == "0.0.0.0" ]] && { echo "$err_msg"; return 1; }
     [[ "$ip" =~ ^(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])){3}$ ]] || { echo "$err_msg"; return 1; }
     for i in ${ip//./ }; do
         [[ "${#i}" -gt 1 && "${i:0:1}" == 0 ]] && { echo "$err_msg"; return 1; }
